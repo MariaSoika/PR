47efa90410c199d6690cfe8c8baa454e311296bc
MariaSoika <mariasoika77@gmail.com>
2023-11-05 23:01:39 +0300

    update Header.h

--- PR1/PR1/Header.h
+++ PR1/PR1/Header.h
@@ -1,1 +1,29 @@
 #pragma once
+#ifndef TIME_H
+#define TIME_H
+
+class Time
+{
+public:
+    int getHours() const;
+    void setHours(int hours1);
+
+    int getMinutes() const;
+    void setMinutes(int minutes1);
+
+    int getSeconds() const;
+    void setSeconds(int seconds1);
+
+    Time(int hours, int minutes, int seconds);
+    Time();
+    Time(const Time& other);
+
+private:
+    int hours;
+    int minutes;
+    int seconds;
+};
+
+void printData(Time time);
+
+#endif
